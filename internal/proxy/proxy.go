376ccaff448770182e61d81ad7311d31c4110abf
Prateet Tiwari <prateettiwari29@gmail.com>
2026-07-17 05:08:10 +0530

    Refactor Proxy initialization to use optimized HTTP transport settings

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -1,10 +1,29 @@
 package proxy
 
 import (
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 )
+
+var optimizedTransport = &http.Transport{
+	Proxy: http.ProxyFromEnvironment,
+	DialContext: (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}).DialContext,
+
+	MaxIdleConns:        10000,
+	MaxIdleConnsPerHost: 2000,
+	MaxConnsPerHost:     0,
+
+	IdleConnTimeout:       90 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+	ForceAttemptHTTP2:     false,
+}
 
 type Proxy struct {
 	reverseProxy *httputil.ReverseProxy
@@ -16,8 +35,11 @@ func New(target string) (*Proxy, error) {
 		return nil, err
 	}
 
+	rp := httputil.NewSingleHostReverseProxy(u)
+	rp.Transport = optimizedTransport
+
 	return &Proxy{
-		reverseProxy: httputil.NewSingleHostReverseProxy(u),
+		reverseProxy: rp,
 	}, nil
 }
 
