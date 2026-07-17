b26114b272528a3e3d868dd4dcfbb22afd814a35
Prateet Tiwari <prateettiwari29@gmail.com>
2026-07-18 03:46:21 +0530

    Implement radix tree insertion logic and update route handling in RadixRouter

--- internal/router/radix.go
+++ internal/router/radix.go
@@ -1,6 +1,10 @@
 package router
 
-import "github.com/Prateet-Github/sentinel/internal/core"
+import (
+	"strings"
+
+	"github.com/Prateet-Github/sentinel/internal/core"
+)
 
 type RadixNode struct {
 	prefix   string
@@ -29,19 +33,40 @@ func (r *RadixRouter) Match(method, path string) (*core.Route, bool) {
 }
 
 func (r *RadixRouter) insert(route core.Route) {
-	panic("not implemented")
+	path := strings.Trim(route.Path, "/")
+	r.insertNode(r.root, path, &route)
 }
 
-// func longestCommonPrefix(a, b string) int {
-// 	n := len(a)
-// 	if len(b) < n {
-// 		n = len(b)
-// 	}
+func longestCommonPrefix(a, b string) int {
+	n := len(a)
+	if len(b) < n {
+		n = len(b)
+	}
 
-// 	i := 0
-// 	for i < n && a[i] == b[i] {
-// 		i++
-// 	}
+	i := 0
+	for i < n && a[i] == b[i] {
+		i++
+	}
 
-// 	return i
-// }
+	return i
+}
+
+func (r *RadixRouter) insertNode(
+	node *RadixNode,
+	path string,
+	route *core.Route,
+) {
+	// TODO: implement radix tree insertion logic here
+
+	for _, child := range node.children {
+		if longestCommonPrefix(child.prefix, path) > 0 {
+			// will handle this next
+			return
+		}
+	}
+
+	node.children = append(node.children, &RadixNode{
+		prefix: path,
+		route:  route,
+	})
+}
