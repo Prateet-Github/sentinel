d49295cb15792b9410b059ee93ee520ae9920c9c
Prateet Tiwari <prateettiwari29@gmail.com>
2026-08-20 04:38:16 +0530

    Refactor HealthChecker to include HTTP client and implement health check logic

--- internal/lb/health.go
+++ internal/lb/health.go
@@ -1,6 +1,11 @@
 package lb
 
-import "github.com/Prateet-Github/sentinel/internal/core"
+import (
+	"net/http"
+	"time"
+
+	"github.com/Prateet-Github/sentinel/internal/core"
+)
 
 type BackendState uint8
 
@@ -9,12 +14,31 @@ const (
 	BackendUnhealthy
 )
 
-type HealthChecker struct{}
+type HealthChecker struct {
+	client *http.Client
+}
 
 func NewHealthChecker() *HealthChecker {
-	return &HealthChecker{}
+	return &HealthChecker{
+		client: &http.Client{
+			Timeout: time.Second,
+		},
+	}
 }
 
 func (h *HealthChecker) Check(backend *core.Backend) bool {
-	return false
+	if backend.HealthCheckPath == "" {
+		return false
+	}
+
+	url := backend.URL + backend.HealthCheckPath
+	resp, err := h.client.Get(url)
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode >= http.StatusOK &&
+		resp.StatusCode < http.StatusBadRequest
+
 }
