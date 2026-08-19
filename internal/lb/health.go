85a88dcd4569bb5210358cb11945855dcd103c18
Prateet Tiwari <prateettiwari29@gmail.com>
2026-08-20 03:25:32 +0530

    Implement HealthChecker with initial Check method and add health_test.go file

--- internal/lb/health.go
+++ internal/lb/health.go
@@ -1,8 +1,20 @@
 package lb
 
+import "github.com/Prateet-Github/sentinel/internal/core"
+
 type BackendState uint8
 
 const (
 	BackendHealthy BackendState = iota
 	BackendUnhealthy
 )
+
+type HealthChecker struct{}
+
+func NewHealthChecker() *HealthChecker {
+	return &HealthChecker{}
+}
+
+func (h *HealthChecker) Check(backend *core.Backend) bool {
+	return false
+}
